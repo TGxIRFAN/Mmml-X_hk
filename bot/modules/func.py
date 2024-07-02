af477421faaa5d8af49dff97a9e2388fba64a8eb
AS TG <126592884+TGxIRFAN@users.noreply.github.com>
2024-07-02 17:32:13 +0530

    Update func.py

--- bot/modules/func.py
+++ bot/modules/func.py
@@ -1,44 +1,133 @@
-#!/usr/bin/env python3
-from re import findall as refindall
-from subprocess import run as srun, check_output as scheck_output
-from os import remove as osremove, rename as osrename, path as ospath
+import os
+import json
+from asyncio import create_subprocess_exec
+from asyncio.subprocess import PIPE
+from bot import LOGGER
 
-from bot import LOGGER, user_data
+async def change_metadata(file, dirpath, key):
+    LOGGER.info(f"Starting metadata modification for file: {file}")
+    temp_file = f"{file}.temp.mkv"
+    full_file_path = os.path.join(dirpath, file)
+    temp_file_path = os.path.join(dirpath, temp_file)
 
+    cmd = [
+        'ffprobe', '-hide_banner', '-loglevel', 'error', '-print_format', 'json', '-show_streams', full_file_path
+    ]
+    process = await create_subprocess_exec(*cmd, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = await process.communicate()
 
-async def edit_video_titles(user_id, file_path):
-    if not file_path.lower().endswith(('.mp4', '.mkv')):
-        return
-    user_dict = user_data.get(user_id, {})
-    if user_dict.get("metadata", False):
-        new_title = user_dict["metadata"]
-        directory = ospath.dirname(file_path)
-        file_name = ospath.basename(file_path)
-        new_file = ospath.join(directory, f"new_{file_name}")
-        LOGGER.info(f"Editing videos metadata title")
-        command_probe = ["ffprobe", "-v", "error", "-show_entries", "stream=index", "-select_streams", "a", "-of", "default=noprint_wrappers=1:nokey=1", file_path]
-        try:
-            probe_result = scheck_output(command_probe).decode("utf-8").strip()
-            audio_stream_count = len(refindall(r'\d+', probe_result))
-        except:
-            audio_stream_count = 0
-        
-        command_probe_subtitles = ["ffprobe", "-v", "error", "-show_entries", "stream=index", "-select_streams", "s", "-of", "default=noprint_wrappers=1:nokey=1", file_path]
-        try:
-            probe_result_subtitles = scheck_output(command_probe_subtitles).decode("utf-8").strip()
-            subtitle_stream_count = len(refindall(r'\d+', probe_result_subtitles))
-        except:
-            subtitle_stream_count = 0
+    if process.returncode != 0:
+        LOGGER.error(f"Error getting stream info: {stderr.decode().strip()}")
+        return file
 
-        cmd = ["ffmpeg", "-i", file_path, "-map", "0", "-c", "copy"]
-        cmd += ["-metadata:s:v:0", f"title={new_title}"]
-        for i in range(audio_stream_count):
-            cmd += ["-metadata:s:a:{}".format(i), f"title={new_title}"]
-        for i in range(subtitle_stream_count):
-            cmd += ["-metadata:s:s:{}".format(i), f"title={new_title}"]
+    try:
+        streams = json.loads(stdout)['streams']
+    except KeyError:
+        LOGGER.error(f"No streams found in the ffprobe output: {stdout.decode().strip()}")
+        return file
 
-        cmd += ["-metadata", f"title={new_title}"]
-        cmd.append(new_file)
-        srun(cmd, check=True)
-        osremove(file_path)
-        osrename(new_file, f"{directory}/{file_name}")
+    cmd = [
+        'xtra', '-y', '-i', full_file_path, '-c', 'copy',
+        '-metadata:s:v:0', f'title={key}',
+        '-metadata', f'title={key}',
+        '-metadata', 'copyright=',
+        '-metadata', 'description=',
+        '-metadata', 'license=',
+        '-metadata', 'LICENSE=',
+        '-metadata', 'author=',
+        '-metadata', 'summary=',
+        '-metadata', 'comment=',
+        '-metadata', 'artist=',
+        '-metadata', 'album=',
+        '-metadata', 'genre=',
+        '-metadata', 'date=',
+        '-metadata', 'creation_time=',
+        '-metadata', 'language=',
+        '-metadata', 'publisher=',
+        '-metadata', 'encoder=',
+        '-metadata', 'SUMMARY=',
+        '-metadata', 'AUTHOR=',
+        '-metadata', 'WEBSITE=',
+        '-metadata', 'COMMENT=',
+        '-metadata', 'ENCODER=',
+        '-metadata', 'FILENAME=',
+        '-metadata', 'MIMETYPE=',
+        '-metadata', 'PURL=',
+        '-metadata', 'ALBUM='
+    ]
+
+    audio_index = 0
+    subtitle_index = 0
+    first_video = False
+
+    for stream in streams:
+        stream_index = stream['index']
+        stream_type = stream['codec_type']
+
+        if stream_type == 'video':
+            if not first_video:
+                cmd.extend(['-map', f'0:{stream_index}'])
+                first_video = True
+            cmd.extend([f'-metadata:s:v:{stream_index}', f'title={key}'])
+        elif stream_type == 'audio':
+            cmd.extend(['-map', f'0:{stream_index}', f'-metadata:s:a:{audio_index}', f'title={key}'])
+            audio_index += 1
+        elif stream_type == 'subtitle':
+            codec_name = stream.get('codec_name', 'unknown')
+            if codec_name in ['webvtt', 'unknown']:
+                LOGGER.warning(f"Skipping unsupported subtitle metadata modification: {codec_name} for stream {stream_index}")
+            else:
+                cmd.extend(['-map', f'0:{stream_index}', f'-metadata:s:s:{subtitle_index}', f'title={key}'])
+                subtitle_index += 1
+        else:
+            cmd.extend(['-map', f'0:{stream_index}'])
+
+    cmd.append(temp_file_path)
+
+    process = await create_subprocess_exec(*cmd, stderr=PIPE, stdout=PIPE)
+    stdout, stderr = await process.communicate()
+
+    if process.returncode != 0:
+        err = stderr.decode().strip()
+        LOGGER.error(err)
+        LOGGER.error(f"Error modifying metadata for file: {file}")
+        return file
+
+    os.replace(temp_file_path, full_file_path)
+    LOGGER.info(f"Metadata modified successfully for file: {file}")
+    return file
+
+async def add_attachment(file, dirpath, attachment_path):
+    LOGGER.info(f"Adding photo attachment to file: {file}")
+
+    temp_file = f"{file}.temp.mkv"
+    full_file_path = os.path.join(dirpath, file)
+    temp_file_path = os.path.join(dirpath, temp_file)
+    
+    attachment_ext = attachment_path.split('.')[-1].lower()
+    if attachment_ext in ['jpg', 'jpeg']:
+        mime_type = 'image/jpeg'
+    elif attachment_ext == 'png':
+        mime_type = 'image/png'
+    else:
+        mime_type = 'application/octet-stream'
+
+    cmd = [
+        'xtra', '-y', '-i', full_file_path,
+        '-attach', attachment_path,
+        '-metadata:s:t', f'mimetype={mime_type}',
+        '-c', 'copy', '-map', '0', temp_file_path
+    ]
+
+    process = await create_subprocess_exec(*cmd, stderr=PIPE, stdout=PIPE)
+    stdout, stderr = await process.communicate()
+
+    if process.returncode != 0:
+        err = stderr.decode().strip()
+        LOGGER.error(err)
+        LOGGER.error(f"Error adding photo attachment to file: {file}")
+        return file
+
+    os.replace(temp_file_path, full_file_path)
+    LOGGER.info(f"Photo attachment added successfully to file: {file}")
+    return file
